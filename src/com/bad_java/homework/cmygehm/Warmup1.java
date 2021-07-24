68435af7c8b2a61a303c14540fe423aac2358541
Artur Gafarov <amgafarov@yoomoney.ru>
2021-07-24 18:34:13 +0300

    homework warmup sleepin

---
+++ src/com/bad_java/homework/cmygehm/Warmup1.java
@@ -0,0 +1,18 @@
+package com.bad_java.homework.cmygehm;
+
+public class Warmup1 {
+    /**
+     * The parameter weekday is true if it is a weekday,
+     * and the parameter vacation is true if we are on vacation.
+     * We sleep in if it is not a weekday or we're on vacation.
+     * Return true if we sleep in.
+     *
+     *
+     * sleepIn(false, false) → true
+     * sleepIn(true, false) → false
+     * sleepIn(false, true) → true
+     */
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+}
