900dd9fb844f01f551d273630663c8257510479e
Nikolai Plokhoi <elefus@yandex.ru>
2021-08-04 19:00:10 +0300

    Code from lecture 2021.08.02

---
+++ src/com/bad_java/lectures/_03/InterfaceExample.java
@@ -0,0 +1,93 @@
+package com.bad_java.lectures._03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+
+public class InterfaceExample {
+
+  public static void main(String[] args) {
+    System.out.println(FixFields4_4.Account);
+
+    Movable movable = new Square(10);
+
+    Square square = (Square) movable;
+    Movable2 movable2 = square;
+    movable = (Movable) movable2;
+
+
+    System.out.println(movable.getStr());
+
+    if (square instanceof Movable) {
+      System.out.println("Yes, it is Movable!");
+    }
+
+    Collection collection = getCollection();
+
+    Square sqr = new Square(10);
+    Rectangle rect = new Rectangle(1, 2);
+    GeometricShape[] shapes = {sqr, rect, new Square(20)};
+
+    class ShapeComparator implements Comparator<GeometricShape> {
+
+      int field;
+
+      @Override
+      public int compare(GeometricShape a, GeometricShape b) {
+        System.out.println("Called compare");
+        return Double.compare(a.calcPerimeter(), b.calcPerimeter());
+      }
+    }
+
+    System.out.println(Arrays.toString(shapes));
+
+    Arrays.sort(shapes, new ShapeComparator());
+
+    System.out.println(Arrays.toString(shapes));
+  }
+
+  private static ArrayList getCollection() {
+    return new ArrayList();
+  }
+}
+
+interface Movable {
+
+  int DELTA = 10;
+
+  Object move(Point2d point);
+
+  Point2d move(int diffX, int diffY);
+
+  String getStr();
+
+  class A {
+
+  }
+}
+
+interface Movable2 {
+
+  int DELTA = 10;
+
+  Point2d move(Point2d point);
+
+  Point2d move(int diffX, int diffY);
+}
+
+
+interface FixFields4_4 {
+
+  int Account = 1;
+  int SourceID = 43;
+
+}
+
+interface ProgramConstants {
+
+  int const_1 = 42;
+  int const_2 = 42;
+  int const_3 = 42;
+
+}
