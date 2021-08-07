ae4a0330bda2157dbe7c98287b4cb841cdf6cb4b
Svetlana Fedorova <svetlana.fedorova@hotmail.com>
2021-08-07 15:31:55 +0300

    3d part of chat bot is ready

--- src/com/bad_java/homework/hyperskill/chat_bot/part_1/SimpleBot.java
+++ src/com/bad_java/homework/hyperskill/chat_bot/part_1/SimpleBot.java
@@ -14,6 +14,16 @@ public class SimpleBot {
         String name = scanner.next();
 
         System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int remainder1 = scanner.nextInt();
+        int remainder2 = scanner.nextInt();
+        int remainder3 = scanner.nextInt();
+
+        int age = (remainder1 * 70 + remainder2 * 21 + remainder3 * 15) % 105;
+
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
 }
 
