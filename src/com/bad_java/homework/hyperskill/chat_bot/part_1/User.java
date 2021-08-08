da0c7cc708a3877fc9c89221af763b0bca63fd1e
Kate <kate.aks8488@gmail.com>
2021-08-08 23:30:46 +0300

    First three stages

---
+++ src/com/bad_java/homework/hyperskill/chat_bot/part_1/User.java
@@ -0,0 +1,15 @@
+public class User {
+    private String userName;
+
+    User(String name){
+        userName = name;
+    }
+
+    public void setUserName(String name) {
+        userName = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
