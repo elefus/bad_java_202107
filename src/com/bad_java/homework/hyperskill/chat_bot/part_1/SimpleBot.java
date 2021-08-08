fb346c36f807d86d52c68b51a77e56a741377206
Krasnikova Mariia <mashakrasnikova@mail.ru>
2021-08-08 18:25:04 +0300

    Added part_1 and part_2 of Simple Chatty Bot

---
+++ src/com/bad_java/homework/hyperskill/chat_bot/part_1/SimpleBot.java
@@ -0,0 +1,8 @@
+package com.bad_java.homework.hyperskill.chat_bot.part_1;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is SimpleJavaBot.");
+        System.out.println("I was created in 2021.");
+    }
+}
