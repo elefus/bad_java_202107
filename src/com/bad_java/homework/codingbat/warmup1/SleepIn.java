1efb44d0080f1d983f4f66cbebb8315960bfd7af
Artur Gafarov <amgafarov@yoomoney.ru>
2021-08-09 19:31:23 +0300

    cs

--- src/com/bad_java/homework/codingbat/warmup1/SleepIn.java
+++ src/com/bad_java/homework/codingbat/warmup1/SleepIn.java
@@ -3,7 +3,8 @@ package com.bad_java.homework.codingbat.warmup1;
 public class SleepIn {
 
   /**
-   * The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
+   * The parameter weekday is true if it is a weekday,
+   * and the parameter vacation is true if we are on vacation.
    * We sleep in if it is not a weekday, or we're on vacation.
    * Return true if we sleep in.
    *
