29bcb3a076cc1f1342f876f2f52f0b8dde20047d
Kirill-mol <kir.mololkin@yandex.ru>
2021-08-11 09:01:24 +0300

    part_3 done

---
+++ src/com/bad_java/homework/hyperskill/coffee_machine/part_3/CoffeeMachine.java
@@ -0,0 +1,17 @@
+package com.bad_java.homework.hyperskill.coffee_machine.part_3;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+
+        Machine machine = new MachineImpl(outputStream, inputStream);
+
+        machine.loadIngredientsAndCalculateCups();
+
+    }
+}
---
+++ src/com/bad_java/homework/hyperskill/coffee_machine/part_3/Machine.java
@@ -0,0 +1,9 @@
+package com.bad_java.homework.hyperskill.coffee_machine.part_3;
+
+public interface Machine {
+	void makeCoffee();
+
+	void calculateIngredients();
+
+	void loadIngredientsAndCalculateCups();
+}
