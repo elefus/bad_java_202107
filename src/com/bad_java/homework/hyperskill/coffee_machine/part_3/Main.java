4f5988ed1350673b76021af6208849414ca39721
strax1e <strax1e@yandex.ru>
2021-08-12 04:53:00 +0300

    Coffee Machine part 3

---
+++ src/com/bad_java/homework/hyperskill/coffee_machine/part_3/Main.java
@@ -0,0 +1,8 @@
+package com.bad_java.homework.hyperskill.coffee_machine.part_3;
+
+public class Main {
+    public static void main(String[] args) {
+        final var coffeeMachine = new CoffeeMachine();
+        coffeeMachine.start();
+    }
+}
