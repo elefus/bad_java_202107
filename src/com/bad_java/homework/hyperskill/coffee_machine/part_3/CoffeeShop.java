582e16779bd3305e7bcff93d61a56075f6be491e
Kate <kate.aks8488@gmail.com>
2021-08-13 20:16:53 +0300

    Was changed class to abstraction

--- src/com/bad_java/homework/hyperskill/coffee_machine/part_3/CoffeeShop.java
+++ src/com/bad_java/homework/hyperskill/coffee_machine/part_3/CoffeeShop.java
@@ -2,7 +2,7 @@ package com.bad_java.homework.hyperskill.coffee_machine.part_3;
 
 public class CoffeeShop {
     public static void main(String[] args) {
-        FirstCoffeeMachine myMachine = new FirstCoffeeMachine();
+        CoffeeMachine myMachine = new FirstCoffeeMachine();
         myMachine.actionSequence();
         myMachine.countingIngredients();
         myMachine.countOfAvailableServings();
