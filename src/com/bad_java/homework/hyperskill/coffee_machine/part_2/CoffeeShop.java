461a82fe96b190812d95298b30442e00d749c72b
Kate <kate.aks8488@gmail.com>
2021-08-13 10:08:17 +0300

    Parts 1 and 2 are done

---
+++ src/com/bad_java/homework/hyperskill/coffee_machine/part_2/CoffeeShop.java
@@ -0,0 +1,9 @@
+package com.bad_java.homework.hyperskill.coffee_machine.part_2;
+
+public class CoffeeShop {
+    public static void main(String[] args) {
+        FirstCoffeeMachine myMachine = new FirstCoffeeMachine();
+        myMachine.actionSequence();
+        myMachine.countingIngredients();
+    }
+}
