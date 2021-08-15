00987047d1e106e5dd8fe9a7d7c1b3494430c556
Alina9730 <schagalieva.lina@yandex.ru>
2021-08-16 00:42:39 +0300

    CoffeeMachine part5.

--- src/com/bad_java/lectures/_03/AnnotationExample.java
+++ src/com/bad_java/lectures/_03/AnnotationExample.java
@@ -5,32 +5,32 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Deprecated(since = "1.4.5", forRemoval = true)
-public class AnnotationExample {
-
-
-  public static void main(String[] args) {
-
-  }
-
-  @Measurable
-  public static void calculate() {
-
-  }
-}
-
-@Retention(RetentionPolicy.CLASS)
-@Target(ElementType.METHOD)
-@interface Measurable {
-
-}
-
-class H {
-
-  @Override
-  public String toString() {
-    return super.toString();
-  }
-}
+//@Deprecated(since = "1.4.5", forRemoval = true)
+//public class AnnotationExample {
+//
+//
+//  public static void main(String[] args) {
+//
+//  }
+//
+//  @Measurable
+//  public static void calculate() {
+//
+//  }
+//}
+//
+//@Retention(RetentionPolicy.CLASS)
+//@Target(ElementType.METHOD)
+//@interface Measurable {
+//
+//}
+//
+//class H {
+//
+//  @Override
+//  public String toString() {
+//    return super.toString();
+//  }
+//}
 
 
