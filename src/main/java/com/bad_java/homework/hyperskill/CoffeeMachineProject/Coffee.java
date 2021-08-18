e8451034c6df82191450d182e17f02e840fea5fd
Svetlana Fedorova <svetlana.fedorova@hotmail.com>
2021-08-18 23:25:05 +0300

    mistake corrected after review

--- src/main/java/com/bad_java/homework/hyperskill/CoffeeMachineProject/Coffee.java
+++ src/main/java/com/bad_java/homework/hyperskill/CoffeeMachineProject/Coffee.java
@@ -1,32 +1,16 @@
 package com.bad_java.homework.hyperskill.CoffeeMachineProject;
 
-public abstract class Coffee {//поставить аннотацию @Data из ломбока, разобраться, почему не ставится
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public abstract class Coffee {
 
     private int waterAmount;
     private int milkAmount;
     private int coffeeBeansAmount;
     private int price;
 
-    public Coffee(int waterAmount, int milkAmount, int coffeeBeansAmount, int price) {
-        this.waterAmount = waterAmount;
-        this.milkAmount = milkAmount;
-        this.coffeeBeansAmount = coffeeBeansAmount;
-        this.price = price;
-    }
 
-    public int getWaterAmount() {
-        return waterAmount;
-    }
-
-    public int getMilkAmount() {
-        return milkAmount;
-    }
-
-    public int getCoffeeBeansAmount() {
-        return coffeeBeansAmount;
-    }
-
-    public int getPrice() {
-        return price;
-    }
 }
