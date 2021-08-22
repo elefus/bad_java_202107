a308aa26d29872d0f37188ea333cc2f556b9bbfd
U_M10PU <iosavinov@alfabank.ru>
2021-08-23 02:18:38 +0300

    Coffee machine started

---
+++ src/com/bad_java/homework/hyperskill/CoffeeMachine/CoffeeMachine1.java
@@ -0,0 +1,33 @@
+package com.bad_java.homework.hyperskill.CoffeeMachine;
+
+public class CoffeeMachine1 {
+  public static void main(String[] args) {
+    System.out.println("Starting to make a coffee");
+    grindingBeans();
+    boilingWater();
+    mixingCoffee();
+    pouringCoffee();
+    pouringMilk();
+    System.out.println("Coffee is ready!");
+  }
+
+  static void grindingBeans(){
+    System.out.println("Grinding coffee beans");
+  }
+
+  static void boilingWater(){
+    System.out.println("Boiling water");
+  }
+
+  static void mixingCoffee(){
+    System.out.println("Mixing boiled water with crushed coffee beans");
+  }
+
+  static void pouringCoffee(){
+    System.out.println("Pouring coffee into the cup");
+  }
+
+  static void pouringMilk(){
+    System.out.println("Pouring some milk into the cup");
+  }
+}
