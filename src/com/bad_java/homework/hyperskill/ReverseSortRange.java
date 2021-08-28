473445a27a8f2a21ac7ce73b2b1e7bd5caf81d3e
Artur Gafarov <amgafarov@yoomoney.ru>
2021-08-28 16:08:20 +0300

    joining array items via `collect(Collectors.joining(" "))`

--- src/com/bad_java/homework/hyperskill/ReverseSortRange.java
+++ src/com/bad_java/homework/hyperskill/ReverseSortRange.java
@@ -1,19 +1,18 @@
 package com.bad_java.homework.hyperskill;
 
 import java.util.Comparator;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ReverseSortRange {
     public static void main(String[] args) {
-        List<Integer> list = IntStream.rangeClosed(0, 9).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-        for (int i: list) {
-            System.out.print(i);
-            if (i == 0) {
-                break;
-            }
-            System.out.print(" ");
-        }
+        String result =
+                IntStream.rangeClosed(0, 9)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .map(String::valueOf)
+                .collect(Collectors.joining(" "))
+        ;
+        System.out.println(result);
     }
 }
