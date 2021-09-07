c10c6db420540c7830011156e1d2629d677b2f1f
1t <rregregret@egfgfg.ru>
2021-09-07 17:05:12 +0300

    Intern/System.exit fix.
    Was refactored:
    1. method "manage()" from interface TaskManager
    2. method "isEnoughSupplies()" from class CoffeeMachine
    3. method "fill()" from class CoffeeMachine
    4. Few names

---
+++ src/main/java/com/bad_java/homework/hyperskill/coffeeMachine/part_5_OOP/machine/ConsoleCoffeeMachine.java
@@ -0,0 +1,13 @@
+package com.bad_java.homework.hyperskill.coffeeMachine.part_5_OOP.machine;
+
+public interface ConsoleCoffeeMachine {
+
+  void buy(Coffee coffee);
+
+  void fill();
+
+  void take();
+
+  void remaining();
+}
+
