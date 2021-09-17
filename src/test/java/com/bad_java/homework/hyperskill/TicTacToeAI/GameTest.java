a4e4f9f8e3a8b972bdf08e5a85adc0b9abfe1c07
Svetlana Fedorova <svetlana.fedorova@hotmail.com>
2021-09-17 18:49:49 +0300

    some tests were added

--- src/test/java/com/bad_java/homework/hyperskill/TicTacToeAI/GameTest.java
+++ src/test/java/com/bad_java/homework/hyperskill/TicTacToeAI/GameTest.java
@@ -118,11 +118,10 @@ class GameTest {
     @ParameterizedTest
     @MethodSource(value = "charParamsGenerator")
     void getWinGameResult(char[] array) {
-        char[] startString = array;
         int n = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                grid[i][j] = startString[n];
+                grid[i][j] = array[n];
                 n++;
             }
         }
