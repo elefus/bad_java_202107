ecdc1bb9721f17335447364d551cea01f30fe028
strax1e <strax1e@yandex.ru>
2021-09-28 00:36:14 +0300

    stack

---
+++ src/com/bad_java/homework/tasks/stack/Stack.java
@@ -0,0 +1,67 @@
+package com.bad_java.homework.tasks.stack;
+
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+
+class Stack<T extends Comparable<T>> {
+    Entry<T> lastPushed;
+    Entry<T> minimal;
+    HashSet<Entry<T>> hashSet = new HashSet<>();
+
+    public void push(T elem) {
+        Entry<T> newElem;
+        if (minimal == null || minimal.element.compareTo(elem) >= 0) {
+            newElem = new Entry<>(elem, minimal, lastPushed);
+            minimal = newElem;
+        } else {
+            newElem = new Entry<>(elem, null, lastPushed);
+        }
+        lastPushed = newElem;
+        hashSet.add(newElem);
+    }
+
+    public T pop() {
+        if (lastPushed == null) {
+            throw new NoSuchElementException();
+        }
+
+        if (lastPushed == minimal) {
+            minimal = minimal.prevMinimalElement;
+        }
+        final var copy = lastPushed;
+        lastPushed = lastPushed.prevPushedElement;
+        hashSet.remove(copy);
+        return copy.element;
+    }
+
+    public T min() {
+        if (minimal == null) {
+            throw new NoSuchElementException();
+        }
+
+        return minimal.element;
+    }
+
+    @Override
+    public String toString() {
+        return hashSet.toString();
+    }
+
+    private static class Entry<T> {
+        private final T element;
+        private final Entry<T> prevMinimalElement;
+        private final Entry<T> prevPushedElement;
+
+        public Entry(T element, Entry<T> prevMinimalElement, Entry<T> prevPushedElement) {
+            this.element = element;
+            this.prevMinimalElement = prevMinimalElement;
+            this.prevPushedElement = prevPushedElement;
+        }
+
+        @Override
+        public String toString() {
+            return element.toString();
+        }
+
+    }
+}
