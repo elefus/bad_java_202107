fe9ea6a47900b6dc46197bd0a9c0be626987e876
Nikolai Plokhoi <elefus@yandex.ru>
2021-10-08 22:09:12 +0300

    Code from lecture 2021.10.08 - lambdas#1

--- src/main/java/com/bad_java/lectures/_12/BasicsFunctional.java
+++ src/main/java/com/bad_java/lectures/_12/BasicsFunctional.java
@@ -1,8 +1,9 @@
 package com.bad_java.lectures._12;
 
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.BiFunction;
 
-public class Test {
+public class BasicsFunctional {
 
     public static void main(String[] args) {
 //        String s1 = somewhatLongOperation1();
@@ -11,17 +12,22 @@ public class Test {
 //        String s3 = concatenate(s1, s2);
 
         // First-class citizen (объекты первого класса)
+        // Side-effect (побочные эффекты)
         // Pure function (чистая функция)
         // High-order functions (функции высшего порядка)
-        BiFunction<Integer, Integer, Integer> adder = Test::add;
-
+        // N-arity (N-арность функций)
+        BiFunction<Integer, Integer, Integer> adder = BasicsFunctional::add;
 
         Integer result4 = adder.apply(1, 2);
         Integer result1 = add(1, 2);
         int result2 = add(2, 3);
         int result3 = add(1, 2);
 
-        BiFunction<Integer, Integer, Integer> result = someMethod(Test::add);
+        BiFunction<Integer, Integer, Integer> result = someMethod(BasicsFunctional::add);
+    }
+
+    public static boolean getBoolean(int param) {
+        return ThreadLocalRandom.current().nextBoolean();
     }
 
     public static Integer add(Integer a, Integer b) {
