00b640cf1d7437b3015dc7de5d35b0628f94fb44
Svetlana Fedorova <svetlana.fedorova@hotmail.com>
2021-10-19 00:33:36 +0300

    ParseString task done

--- src/test/java/com/bad_java/lectures/_12/ParseStringTest.java
+++ src/test/java/com/bad_java/lectures/_12/ParseStringTest.java
@@ -8,7 +8,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
