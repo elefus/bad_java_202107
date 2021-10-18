52c91f7c16a443128822b5c99c11b2135614f043
Svetlana Fedorova <svetlana.fedorova@hotmail.com>
2021-10-18 15:45:57 +0300

    ParseString task done

--- src/test/java/com/bad_java/lectures/_12/ParseStringTest.java
+++ src/test/java/com/bad_java/lectures/_12/ParseStringTest.java
@@ -1,34 +1,51 @@
 package com.bad_java.lectures._12;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
 
 public class ParseStringTest {
 
     /**
-     * Выбирает из текста наиболее часто встречающиеся слова.
-     * Подсчет слов выполняется без учета их регистра, т.е. "Привет", "привет", "пРиВеТ" - одно и то же слово.
-     * Если некоторые слова имеют одинаковую частоту, то в выходном списке они упорядочиваются в лексикографическом порядке.
-     * @param text Исходный текст в котором слова (в смешанном регистре) разделены пробелами.
-     * @param numberWords Количество наиболее часто встречающихся слов, которые необходимо отобрать.
+     * Выбирает из текста наиболее часто встречающиеся слова. Подсчет слов выполняется без учета их
+     * регистра, т.е. "Привет", "привет", "пРиВеТ" - одно и то же слово. Если некоторые слова имеют
+     * одинаковую частоту, то в выходном списке они упорядочиваются в лексикографическом порядке.
+     *
+     * @param text        Исходный текст в котором слова (в смешанном регистре) разделены
+     *                    пробелами.
+     * @param numberWords Количество наиболее часто встречающихся слов, которые необходимо
+     *                    отобрать.
      * @return Список отобранных слов (в нижнем регистре).
      */
     private List<String> getFrequentlyOccurringWords(String text, int numberWords) {
-        throw new UnsupportedOperationException();
+
+        return Arrays.stream(text.split("\\W+"))
+                     .map(String::toLowerCase)
+                     .collect(Collectors.groupingBy(Function.identity(),
+                         Collectors.counting())).entrySet().stream()
+                     .sorted(Map.Entry.<String, Long>comparingByValue().reversed()
+                                      .thenComparing(Map.Entry.comparingByKey()))
+                     .map(Entry::getKey).limit(numberWords)
+                     .collect(Collectors.toList());
     }
 
     @Test
     void test2() {
-        String source = "Lorem  ipsum dolor sit,    amet consectetur adipiscing elit Sed sodales consectetur purus at "
+        String source =
+            "Lorem  ipsum dolor sit,    amet consectetur adipiscing elit Sed sodales consectetur purus at "
                 + "faucibus Donec mi quam tempor vel ipsum non faucibus suscipit massa Morbi lacinia velit "
                 + "blandit tincidunt efficitur Vestibulum. eget metus imperdiet sapien laoreet faucibus Nunc "
                 + "eget vehicula mauris ac auctor lorem Lorem ipsum dolor sit amet consectetur adipiscing elit "
                 + "Integer vel odio nec mi tempor dignissim";
 
         List<String> result = getFrequentlyOccurringWords(source, 10);
-        assertThat(result).containsExactly("consectetur", "faucibus", "ipsum", "lorem", "adipiscing", "amet", "dolor", "eget", "elit", "mi");
+        assertThat(result).containsExactly("consectetur", "faucibus", "ipsum", "lorem",
+            "adipiscing", "amet", "dolor", "eget", "elit", "mi");
     }
 }
